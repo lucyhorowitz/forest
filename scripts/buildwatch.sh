78b3e1a196028f26e0a1c11b61ccc3fa34b1affa
Lucy Horowitz <111657652+lucyhorowitz@users.noreply.github.com>
2025-06-16 13:32:18 -0700

    reorganization

--- scripts/buildwatch.sh
+++ scripts/buildwatch.sh
@@ -1,1 +1,3 @@
-fswatch -or trees role | xargs -n1 -I{} sh -c './forester build --dev && rsync -az --delete output/~lucy/* lucy@login.math.berkeley.edu:~/public_html/'
+#!/usr/bin/env bash
+umask 022
+fswatch -or trees role | xargs -n1 -I{} sh -c './forester build --dev && rsync -az --chmod=Du=rwx,Dg=rx,Do=rx,Fu=rw,Fg=r,Fo=r --delete output/~lucy/ lucy@login.math.berkeley.edu:~/public_html/forest/'
--- scripts/new.sh
+++ scripts/new.sh
@@ -4,12 +4,18 @@ code=`basename $var .tree`
 echo "" >> $var
 echo "\title{}" >> $var
 if [ $1 = "ref" ]; then 
+  echo '\import{macros}' >> $var
+  echo '\import{base-macros}' >> $var
   echo '\taxon{reference}' >> $var
   echo '\author{}' >> $var
 elif [ $1 = "q" ]; then 
+  echo '\import{macros}' >> $var
+  echo '\import{base-macros}' >> $var
   echo '\taxon{quote}' >> $var
   echo '\author{}' >> $var
 elif [ $1 = "def" ]; then 
+  echo '\import{macros}' >> $var
+  echo '\import{base-macros}' >> $var
   echo '\taxon{definition}' >> $var
   echo '\author{}' >> $var
 fi

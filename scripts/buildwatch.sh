0a98be031c03af3a4354ef41fb4a332f8e67b644
Lucy Horowitz <111657652+lucyhorowitz@users.noreply.github.com>
2025-06-17 08:58:13 -0700

    fix website problems

--- scripts/buildwatch.sh
+++ scripts/buildwatch.sh
@@ -1,3 +1,7 @@
 #!/usr/bin/env bash
 umask 022
-fswatch -or trees role | xargs -n1 -I{} sh -c './forester build --dev && rsync -az --chmod=Du=rwx,Dg=rx,Do=rx,Fu=rw,Fg=r,Fo=r --delete output/~lucy/ lucy@login.math.berkeley.edu:~/public_html/forest/'
+fswatch -or trees role | xargs -n1 -I{} sh -c \
+  './forester build --dev && \
+   rsync -az --delete \
+         --chmod=Du=rwx,Dg=rx,Do=rx,Fu=rw,Fg=r,Fo=r \
+         output/~lucy/forest/ lucy@login.math.berkeley.edu:~/public_html/forest/'
--- scripts/new.sh
+++ scripts/new.sh
@@ -3,20 +3,19 @@ var=`./forester new --dest=trees/$1 --prefix=$1`
 code=`basename $var .tree`
 echo "" >> $var
 echo "\title{}" >> $var
+echo '\import{macros}' >> $var
+echo '\import{base-macros}' >> $var
+echo '\import{rel}' >> $var
 if [ $1 = "ref" ]; then 
-  echo '\import{macros}' >> $var
-  echo '\import{base-macros}' >> $var
   echo '\taxon{reference}' >> $var
   echo '\author{}' >> $var
 elif [ $1 = "q" ]; then 
-  echo '\import{macros}' >> $var
-  echo '\import{base-macros}' >> $var
   echo '\taxon{quote}' >> $var
   echo '\author{}' >> $var
 elif [ $1 = "def" ]; then 
-  echo '\import{macros}' >> $var
-  echo '\import{base-macros}' >> $var
   echo '\taxon{definition}' >> $var
   echo '\author{}' >> $var
+elif [ $1 != "def" ]; then
+  echo '\taxon{}' >> $var 
 fi
 echo $var

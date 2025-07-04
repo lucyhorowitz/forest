0d8f0aaf2ea68306c174e5d8c967f7c6603f3e89
Lucy Horowitz <111657652+lucyhorowitz@users.noreply.github.com>
2025-07-03 18:39:28 -0700

    july 3

--- scripts/buildwatch.sh
+++ scripts/buildwatch.sh
@@ -3,9 +3,6 @@ umask 022
 fswatch -or trees | xargs -n1 -I{} sh -c \
   './forester build && \
    rsync -az --delete \
-         --exclude='\''daily/'\'' --exclude='\''meta/'\'' --exclude='\''priv/'\'' \
+         --exclude='\''*/daily*/'\'' --exclude='\''*/meta*/'\'' --exclude='\''*/priv*/'\'' \
          --chmod=Du=rwx,Dg=rx,Do=rx,Fu=rw,Fg=r,Fo=r \
-         output/~lucy/forest/ lucy@login.math.berkeley.edu:~/public_html/forest/ && \
-   rsync -az \
-         --chmod=Fu=rw,Fg=r,Fo=r \
-         output/forest/*.xsl lucy@login.math.berkeley.edu:~/public_html/forest/'
+         output/~lucy/forest/ lucy@login.math.berkeley.edu:~/public_html/forest/'
